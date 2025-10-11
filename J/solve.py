659a0cded4e7272bea3587811454f78eec40dce9
julien <julienyang12@gmail.com>
2025-10-11 17:43:57 -0400

    fixed j

--- J/solve.py
+++ J/solve.py
@@ -3,8 +3,11 @@ import time
 
 def solve(input):
     # 1, 10, 11, ..., 19, 21, 31, 41, 51, 61, 71, 81, 91, 100
-    
-    sys.stdout.write(input.split()[-1][-1])
+    lastnum = input.split()[-1]
+    if lastnum[-1] == "0":
+        sys.stdout.write(lastnum[0])
+    else:
+        sys.stdout.write(lastnum[-1])
     #print(input.split()[-1][-1])
 
 if __name__ == "__main__":
