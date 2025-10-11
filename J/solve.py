a80ebe3a5d9d1b0f733921b154e80fa8ab393068
julien <julienyang12@gmail.com>
2025-10-11 18:03:28 -0400

    10 bullshit

--- J/solve.py
+++ J/solve.py
@@ -5,10 +5,7 @@ def solve(input):
     # 1, 10, 11, ..., 19, 21, 31, 41, 51, 61, 71, 81, 91, 100
     lastnum = input.split()[-1]
     if lastnum[-1] == "0":
-        if lastnum[0] == "1":
-            sys.stdout.write("10")
-        else:
-            sys.stdout.write(lastnum[0])
+        sys.stdout.write("10")
     else:
         sys.stdout.write(lastnum[-1])
     #print(input.split()[-1][-1])
